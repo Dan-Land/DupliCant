39be9c40b9bc353f948eb64f8817e38addfadd64
Dan C <143655990+Dan-Land@users.noreply.github.com>
2026-07-19 18:50:40 -0700

    Pseudocode for basic file size gathering for potential duplicate candidates

--- duplicant.py
+++ duplicant.py
@@ -1,8 +1,33 @@
 """DupliCant: a command-line duplicate file finder."""
+"""Copyright (c) 2026 Daniel Coffland"""
 
 def main() -> None:
     """Run the application"""
     print("DupliCant is under construction.")
+
+    # get directory path from command line
+
+    # if path does not exist:
+        # display error
+        # stop program
+    
+    # if path is not a directory:
+        # display error
+        # stop program
+    
+    # create empty dictionary called files_by_size
+
+    # for each file found recursively:
+        # try:
+            # get file size
+            # add file path to list associated with that size
+        # except file access error:
+            # report or record skipped file
+    
+    # for each size and path list:
+        # if path list contains more than one file:
+            # display the size
+            # dispaly each path
 
 if __name__ == "__main__":
     main()
